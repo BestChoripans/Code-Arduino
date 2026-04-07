03aa40297d5a5f15fc7b94c2313f7d7e8b1578e8
BestChori <134541919+BestChoripans@users.noreply.github.com>
2026-04-07 12:53:33 -0400

    Optimize output by removing Serial.print calls
    Removed Serial.print statements to streamline output.

--- funciones.ino
+++ funciones.ino
@@ -9,23 +9,15 @@ void ReadQTR() {
   for(int i=0;i<8;i++){
     if(sensorValues[i]>umbral){
       SerialBT.print("□ (");
-      Serial.print("□ (");
       SerialBT.print(sensorValues[i]);
-      Serial.print(sensorValues[i]);
       SerialBT.print(")");
-      Serial.print(")");
     } else {
       SerialBT.print("■ (");
-      Serial.print("■ (");
       SerialBT.print(sensorValues[i]);
-      Serial.print(sensorValues[i]);
       SerialBT.print(")");
-      Serial.print(")");
     } 
     SerialBT.print("\t");
-    Serial.print("\t");
   }
-  SerialBT.println();
   delay(100);
 }
 
